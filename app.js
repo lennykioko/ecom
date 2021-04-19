6ac9a1f4bddef8287cd53087d8a8a3562f65bd47
lennykioko <lennykmutua@gmail.com>
2021-04-19 21:57:22 +0300

    Update fs

---
+++ app.js
@@ -0,0 +1,143 @@
+const BASE_URL = `http://localhost:1337`
+let products;
+
+const mainContent = document.querySelector("#mainContent");
+
+const searchForm1 = document.querySelector("#searchForm1");
+const searchInput1 = document.querySelector("#searchInput1");
+
+const searchForm2 = document.querySelector("#searchForm2");
+const searchInput2 = document.querySelector("#searchInput2");
+
+const quickview = document.querySelector("#quickview-wrapper");
+
+
+searchForm1.addEventListener("submit", (event) => {
+    event.preventDefault();
+    handleSearch(searchInput1.value);
+});
+
+searchForm2.addEventListener("submit", (event) => {
+    event.preventDefault();
+    handleSearch(searchInput2.value);
+});
+
+function commaThousand(num) {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+const loader = `<div class="mx-auto vh-100 text-center d-flex justify-content-center">
+  <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+    <span class="sr-only">Loading...</span>
+  </div>
+</div>`;
+
+
+const getSearchItems = async (search) => {
+    mainContent.innerHTML = loader;
+
+    try {
+        let productsUrl = new URL(`${BASE_URL}/products/`);
+        let url;
+
+        if (search && search.length > 0) {
+          url = `${productsUrl}?description_contains=${search}`
+        } else {
+          url = `${productsUrl}`
+        }
+
+        const res = await fetch(url);
+        mainContent.innerHTML = "";
+        return res.json();
+    } catch(err) {
+        console.log(err);
+    }
+};
+
+const handleSearch = async (search) => {
+    const response = await getSearchItems(search);
+    const data = response
+    products = response
+
+    data?.map((item) => {
+      mainContent.insertAdjacentHTML(
+          "beforeend",
+          `<div class="col-lg-4 col-md-6">
+              <div class="product-item">
+                  <div class="product-img">
+                      <a href="#" data-toggle="modal" onclick="setProduct(${item})" data-target="#productModal">
+                          <img src=${BASE_URL}${item.image?.url} alt=""/>
+                      </a>
+                  </div>
+                  <div class="product-info">
+                      <h6 class="product-title">
+                          <a href="#" data-toggle="modal" onclick="setProduct(${item})" data-target="#productModal">${item.name} </a>
+                      </h6>
+                      <div class="pro-rating">
+                          <a href="#"><i class="zmdi zmdi-star"></i></a>
+                          <a href="#"><i class="zmdi zmdi-star"></i></a>
+                          <a href="#"><i class="zmdi zmdi-star"></i></a>
+                          <a href="#"><i class="zmdi zmdi-star-half"></i></a>
+                          <a href="#"><i class="zmdi zmdi-star-outline"></i></a>
+                      </div>
+                      <h3 class="pro-price">KSh ${item.price}</h3>
+                      <ul class="action-button">
+                          <li>
+                              <a href="#" data-toggle="modal" onclick="setProduct(${item.id})" class="read-more" data-target="#productModal" title="Read More">
+                                  <i class="zmdi zmdi-eye"></i>
+                              </a>
+                          </li>
+                      </ul>
+                  </div>
+              </div>
+          </div>`
+        );
+    });
+};
+
+
+const setProduct = async (item_id) => {
+    let item = products.filter(item => item.id == item_id);
+    item = item[0]
+    quickview.innerHTML =
+      `<div class="modal fade" id="productModal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body">
+              <div class="modal-product clearfix">
+                  <div class="product-images">
+                    <div class="main-image images">
+                        <img alt="" src=${BASE_URL}${item.image?.url}>
+                    </div>
+                  </div><!-- .product-images -->
+
+                  <div class="product-info">
+                    <h1>${item.name}</h1>
+                    <div class="price-box-3">
+                        <div class="s-price-box">
+                            <span class="new-price">KSh ${item.price}</span>
+                            <span class="old-price">KSh ${item.old_price}</span>
+                        </div>
+                    </div>
+                    <a href="tel:0722650647" class="see-all font-weight-bold ">0722 650 647</a>
+                    <div class="quick-add-to-cart">
+                        <button class="single_add_to_cart_button" type="button">Call Now</buton>
+                    </div>
+                    <div class="quick-desc">
+                        ${item.description}
+                    </div>
+
+                  </div><!-- .product-info -->
+                </div><!-- .modal-product -->
+            </div><!-- .modal-body -->
+            </div><!-- .modal-content -->
+        </div><!-- .modal-dialog -->
+    </div>`;
+};
+
+
+// empty search returns all
+handleSearch();
