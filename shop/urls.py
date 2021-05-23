27689663cf4d8b9d7ce45a015b44f36d8e1ca85b
ero <eric.ndira@gmail.com>
2021-05-23 12:33:18 +0300

    added dynamic links

--- shop/urls.py
+++ shop/urls.py
@@ -7,7 +7,7 @@ app_name = 'shop'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
     path('ajax/', views.AjaxSearchView.as_view(), name='ajax'),
-    path('about', views.AboutView.as_view(), name='about'),
-    path('contact', views.ContactView.as_view(), name='contact'),
-    path('shop', views.ShopView.as_view(), name='shop')
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
+    path('shop/', views.ShopView.as_view(), name='shop')
 ]
